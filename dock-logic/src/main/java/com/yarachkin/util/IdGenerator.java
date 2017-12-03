6ba23062009ac8caae4f878ed2d05165558c1ac3
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-12-03 17:39:49 +0300

    Create an id generator

---
+++ dock-logic/src/main/java/com/yarachkin/util/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.yarachkin.util;
+
+public class IdGenerator {
+    private static long shipId = 0;
+
+    private static boolean isTest = false;
+
+    private IdGenerator() {
+    }
+
+    public static void setIsTest(boolean isTest) {
+        IdGenerator.isTest = isTest;
+    }
+
+    public static long generateShipId() {
+        return isTest ? 1L : ++shipId;
+    }
+}
