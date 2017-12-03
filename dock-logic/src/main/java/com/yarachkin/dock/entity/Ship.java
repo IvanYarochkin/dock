6d438df00c4bbec4d2c10e5f65db1f21491f5481
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-12-03 22:15:48 +0300

    Remove get methods in a Ship

--- dock-logic/src/main/java/com/yarachkin/dock/entity/Ship.java
+++ dock-logic/src/main/java/com/yarachkin/dock/entity/Ship.java
@@ -7,40 +7,20 @@ public class Ship extends Thread {
     private int loadingContainersCounts;
     private int uploadingContainersCounts;
 
-    public long getShipId() {
-        return shipId;
-    }
-
     public void setShipId(long shipId) {
         this.shipId = shipId;
-    }
-
-    public int getCapacity() {
-        return capacity;
     }
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
-    public int getBoardContainersCounts() {
-        return boardContainersCounts;
-    }
-
     public void setBoardContainersCounts(int boardContainersCounts) {
         this.boardContainersCounts = boardContainersCounts;
     }
 
-    public int getUploadingContainersCounts() {
-        return uploadingContainersCounts;
-    }
-
     public void setUploadingContainersCounts(int uploadingContainersCounts) {
         this.uploadingContainersCounts = uploadingContainersCounts;
-    }
-
-    public int getLoadingContainersCounts() {
-        return loadingContainersCounts;
     }
 
     public void setLoadingContainersCounts(int loadingContainersCounts) {
