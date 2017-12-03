1953cc0ce36b16deb57c84c85bb79a505f2bc770
IvanYarochkin <Ivan.Yarochkin97@gmail.com>
2017-12-03 21:19:00 +0300

    Add a dock class

---
+++ dock-logic/src/main/java/com/yarachkin/dock/entity/Dock.java
@@ -0,0 +1,33 @@
+package com.yarachkin.dock.entity;
+
+public class Dock {
+    private int capacity;
+    private int dockContainersCounts;
+
+    private Dock() {
+    }
+
+    private static class SingletonHolder {
+        private static final Dock INSTANCE = new Dock();
+    }
+
+    public static Dock getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getDockContainersCounts() {
+        return dockContainersCounts;
+    }
+
+    public void setDockContainersCounts(int dockContainersCounts) {
+        this.dockContainersCounts = dockContainersCounts;
+    }
+}
